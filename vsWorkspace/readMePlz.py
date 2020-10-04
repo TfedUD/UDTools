3047a22bec0a6e96b87f857955e9b6a61ef7ca9a
TfedUD <63176386+TfedUD@users.noreply.github.com>
2020-10-04 14:34:09 -0400

    Create readMePlz.py

---
+++ vsWorkspace/readMePlz.py
@@ -0,0 +1,5 @@
+Hi. This is the start of a thing, that will hopefully be helpful.
+Currently working on a 'core component' do handle 2020 National Cost Estimator Data.
+After the 'Core' then:   'call from' and geometric interogation ect.
+
+Why do massing and energy modeling and the likes without a $$ parameter in the mix?   
